c5f7d3ed9643c1c424d5e1b7a0990ce381819765
jangwon kim <seed0335@gmail.com>
2023-05-18 10:07:35 +0900

    5월 18일 오전 회의 결과 데이터 보내기

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
+# 맥 사용자는 풀어서 사용하세요 - 본인 mongoDB로 변경하기
+# import certifi
+# ca = certifi.where()
+
 # mongoDB는 김장원 - 본인 mongoDB로 변경하기
 from pymongo import MongoClient
-
-client = MongoClient('mongodb+srv://sparta:test@cluster0.ziorpfn.mongodb.net/?retryWrites=true&w=majority')
+# 김장원mongoDB
+client = MongoClient('mongodb+srv://sparta:test@cluster0.rsr8xyc.mongodb.net/?retryWrites=true&w=majority')
+# 김희석mongoDB
+# client = MongoClient('')
+# 이은비mongoDB
+# client = MongoClient('')
+# 이현경mongoDB
+# client = MongoClient('')
+# 임수영mongoDB
+# client = MongoClient('')
 db = client.dbsparta
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+# 임수영님 서브 페이지 동작 관련 코드
 @app.route("/myprofile/new", methods=["GET"])
 def post_profile():
 
@@ -33,7 +46,7 @@ def test_get():
     return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
 
 
-# 방명록 저장하는 곳
+# 방명록 저장하는 곳(김희석)
 @app.route('/guestbook', methods=['POST'])
 def guestbook_post():
    name_receive = request.form['name_give']
@@ -45,7 +58,7 @@ def guestbook_post():
    db.guestbook_comments.insert_one(doc)
    return jsonify({'msg': '방명록이 등록되었습니다!'})
 
-# 방명록 mongDB에서 index.html로 데이터 전송
+# 방명록 mongDB에서 index.html로 데이터 전송(김희석)
 @app.route("/guestbook", methods=["GET"])
 def guestbook_get():
     all_comments = list(db.guestbook_comments.find({},{'_id':False}))
--- wirte1.py
+++ wirte1.py
@@ -1,5 +1,8 @@
 # from pymongo import MongoClient
 
+# import certifi
+# ca = certifi.where()
+
 # client = MongoClient('mongodb+srv://sparta:test@cluster0.rsr8xyc.mongodb.net/?retryWrites=true&w=majority')
 # db = client.dbsparta
 
@@ -15,4 +18,5 @@
 # }
 # db.profiles.insert_one(doc) 
 
+# 김희석님 본인 데이터 저장해주세요. (위에 주소 본인DB로 변경 한 후 테스트하기)
 # DB에 profiles가 없어서 오류가 나면 이 코드 주석 풀고 터미널 실행하기 (더미데이터)
